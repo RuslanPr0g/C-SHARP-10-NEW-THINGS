a346c4e4ccff5a762a0b7c0dfae8c339b170df2a
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-11-16 12:50:19 +0200

    Constant interpolated strings

---
+++ 10-new-things-cisharp/10-new-things-cisharp/Constant interpolated strings.cs
@@ -0,0 +1,14 @@
+﻿// Before, this feature was not supported. C# 10 allows you to use string interpolation for constant strings:
+//const string constStrFirst = "FirstStr";
+//const string summaryConstStr = $"SecondStr {constStrFirst}";
+
+// Interesting fact. This change relates only to string interpolation for constant strings,
+// i.e the addition of a constant character is not allowed:
+
+//const char a = 'a';
+//const string constStrFirst = "FirstStr";
+//const string summaryConstStr = $"SecondStr {constStrFirst} {a}";
+// Error CS0133
+// The expression being assigned to
+// 'summaryConstStr' must be constant
+
